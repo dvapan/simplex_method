5ca8987cf34bb75421c32243ef5b7d747f7e78ce
dvapan <dvapan@gmail.com>
2015-06-02 11:53:22 +0800

    task-generator

---
+++ tasks_generator.py
@@ -0,0 +1,20 @@
+__author__ = 'dvapan'
+
+import random
+import scipy as sc
+
+from simplex_method import solver
+
+min_val = -50
+max_val = 50
+
+def generate_task(m,n):
+    A = sc.matrix([[random.randint(min_val, max_val)
+         for i in range(n)] for j in range(m)])
+    b = sc.matrix([random.randint(1, max_val) for i in range(m)]).transpose()
+    c = sc.matrix([random.randint(min_val, max_val) for i in range(n)]).transpose()
+    return A, b, c
+
+Task = generate_task(4, 4)
+print Task
+print solver(Task[0], Task[1], Task[2], 0.01)
